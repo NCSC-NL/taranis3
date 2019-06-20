922696da460d4213fed4dbec67eda5f61ec6f6da
Mark Overmeer <mark@overmeer.net>
2019-06-20 12:26:25 +0200

    start release 3.6.3

--- pm/Taranis.pm
+++ pm/Taranis.pm
@@ -30,7 +30,7 @@ use File::Path  qw(make_path);
 use Taranis::FunctionalWrapper qw(CGI);
 
 our @ISA = qw(Exporter);
-our $VERSION = '3.6.2';
+our $VERSION = '3.6.3';
 
 our %EXPORT_TAGS = ();
 $EXPORT_TAGS{util} = [
